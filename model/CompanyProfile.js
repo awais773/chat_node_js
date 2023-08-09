b23dfca6ec7c189bd5412de708a35025e25345f2
chandjr0 <chandjr0@gmail.com>
2023-08-09 17:07:46 +0500

    types change

--- model/CompanyProfile.js
+++ model/CompanyProfile.js
@@ -6,7 +6,7 @@ const User = require('./User'); // Adjust the path based on your project structu
 const CompanyProfile = sequelize.define('company_profiles', {
  
   user_id: {
-    type: DataTypes.INTEGER,
+    type: DataTypes.UUID,
     allowNull: true,
     references: {
       model: User, // Reference the User model
