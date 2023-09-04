b0f17bad7daebcc4dc8f3b2b49e1be364eb8308f
chandjr0 <chandjr0@gmail.com>
2023-09-04 12:37:28 +0500

    ststus codes

--- controller/PortfolioController.js
+++ controller/PortfolioController.js
@@ -5,13 +5,25 @@ async function create(req, res, next) {
   try {
     const { body } = req;
     const { userId } = req;
-    const Portfolio = await PortfolioServices.create({ ...body , user_id: userId });
-    res.status(200).json({
+    const Portfolio = await PortfolioServices.create({ ...body, user_id: userId });
+
+    if (!Portfolio) {
+      // If the portfolio creation was not successful, return a 400 Bad Request status.
+      return res.status(400).json({
+        success: false,
+        message: "Portfolio creation failed",
+      });
+    }
+
+    // If the portfolio was created successfully, return a 201 Created status.
+    return res.status(201).json({
       success: true,
-      data: Portfolio
+      data: Portfolio,
     });
   } catch (error) {
-    res.json({
+    // Handle other errors with a 500 Internal Server Error status.
+    return res.status(500).json({
+      success: false,
       message: error.message,
     });
   }
@@ -19,73 +31,102 @@ async function create(req, res, next) {
 
 
 
-async function update (req, res)  {
+async function update(req, res) {
   const Id = req.params.Id; // Get the user ID from the route parameter
   const updates = req.body; // The updates will be sent in the request body as JSON
   try {
     const portfolio = await PortfolioServices.update(Id, updates);
-    res.json({ success: true, data: portfolio });
+
+    if (!portfolio) {
+      // If the update operation didn't find a matching portfolio, return a 404 Not Found status.
+      return res.status(404).json({ success: false, message: 'Portfolio not found' });
+    }
+
+    // If the portfolio was updated successfully, return a 200 OK status.
+    return res.json({ success: true, data: portfolio });
   } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
+    // Handle other errors with a 500 Internal Server Error status.
+    return res.status(500).json({ success: false, error: error.message });
   }
 }
 
 
-async function find (req, res)  {
+async function find(req, res) {
   const Id = req.params.Id; // Get the user ID from the route parameter
   try {
     const portfolio = await PortfolioServices.find(Id);
-    res.json({ success: true, data: portfolio });
+
+    if (!portfolio) {
+      // If no portfolio is found with the given ID, return a 404 Not Found status.
+      return res.status(404).json({ success: false, message: 'Portfolio not found' });
+    }
+
+    // If the portfolio is found, return a 200 OK status with the portfolio data.
+    return res.json({ success: true, data: portfolio });
   } catch (error) {
-    res.json({
-      success: false,
-      error: error.message,
-  });  }
+    // Handle other errors with a 500 Internal Server Error status.
+    return res.status(500).json({ success: false, error: error.message });
+  }
 }
-
-
 
 async function get(req, res, next) {
   try {
-    const {userId} = req;
-    const portfolio = await PortfolioServices.get(userId);
-    res.status(200).json({
+    const { userId } = req; // Extract the user ID from the request object
+    const portfolio = await PortfolioServices.get(userId); // Call PortfolioServices to get the portfolio for the user
+
+    if (!portfolio) {
+      // If no portfolio is found for the user, return a 404 Not Found status.
+      return res.status(404).json({
+        success: false,
+        message: 'Portfolio not found for this user',
+      });
+    }
+
+    // If a portfolio is found, respond with a 200 OK status and the portfolio data.
+    return res.status(200).json({
       success: true,
-      data: portfolio
+      data: portfolio,
     });
   } catch (error) {
-    res.json({
+    // Handle other errors with a 500 Internal Server Error status.
+    return res.status(500).json({
       success: false,
       error: error.message,
     });
   }
 }
 
-
 async function portfolioDelete(req, res, next) {
-  const { id } = req.params;
+  const { id } = req.params; // Get the portfolio ID from the route parameter
   try {
-    const result = await PortfolioServices.portfolioDelete(id);
-    if (result) {
-      res.status(200).json({
-        success: true,
-        message: 'delete Sucessfully'
-      });
-    } else {
-      res.json({
-        message: "portfolio not found",
+    const result = await PortfolioServices.portfolioDelete(id); // Call PortfolioServices to delete the portfolio
+
+    if (result === null) {
+      // If no portfolio was found to delete, respond with a 404 Not Found status.
+      return res.status(404).json({
+        success: false,
+        message: 'Portfolio not found',
       });
     }
+
+    // If the portfolio was deleted successfully, respond with a 200 OK status.
+    return res.status(200).json({
+      success: true,
+      message: 'Portfolio deleted successfully',
+    });
   } catch (error) {
-    next(error);
+    // Handle other errors with a 500 Internal Server Error status.
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
   }
 }
 
-
 module.exports = {
-   create,
-    get,
-    update,
-    find,
-    portfolioDelete,
+  create,
+  get,
+  update,
+  find,
+  portfolioDelete,
 }
--- services/PortfolioServices.js
+++ services/PortfolioServices.js
@@ -3,53 +3,65 @@ const Portfolio = require("../model/Portfolio");
 
 exports.create = async (body) => {
   try {
-    debugger
     const data = await Portfolio.create({ ...body });
     return data;
   } catch (error) {
-    debugger
-    // const errors = error.errors.map((item) => ({ message: item.message }));
-    return error.message
+    // Check if the error is a Sequelize validation error
+    if (error.name === 'SequelizeValidationError') {
+      const validationErrors = error.errors.map((item) => ({ message: item.message }));
+      return { error: 'Validation Error', details: validationErrors };
+    } else {
+      // For other errors, return a generic error message
+      return { error: 'Database Error', message: error.message };
+    }
   }
 };
+
 
 
 exports.get = async (userId) => {
+  // Use Sequelize's findAll method to query the database for portfolios
   const portfolio = await Portfolio.findAll({
     where: {
-      user_id: userId,
+      user_id: userId, // Filter portfolios based on the provided userId
     },
   });
-  return portfolio;
+  return portfolio; // Return the array of portfolio objects
 };
 
 
-exports.find = async (Id,) => {
+exports.find = async (Id) => {
   try {
     const portfolio = await Portfolio.findByPk(Id);
     if (!portfolio) {
-      throw new Error('portfolio not found');
+      // If no portfolio is found with the given ID, return null to indicate that the resource doesn't exist.
+      return null;
     }
-    return portfolio;    
+    return portfolio;
   } catch (error) {
-    return error.message
+    // Handle any errors that occur during the find operation.
+    return error.message;
   }
 };
+
 
 
 exports.update = async (Id, updates) => {
   try {
     const portfolio = await Portfolio.findByPk(Id);
     if (!portfolio) {
-      throw new Error('User not found');
+      // If no portfolio is found with the given ID, return null to indicate that the resource doesn't exist.
+      return null;
     }
     Object.assign(portfolio, updates);
     await portfolio.save();
-    return portfolio;    
+    return portfolio;
   } catch (error) {
-    return error.message
+    // Handle any errors that occur during the update process.
+    return error.message;
   }
 };
+
 
 
 
