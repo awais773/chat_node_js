9cb5c73ce7849e3413c6e88e5aff82de52fcd836
chandjr0 <chandjr0@gmail.com>
2023-09-22 19:29:55 +0500

    push

--- index.js
+++ index.js
@@ -32,56 +32,56 @@ wss.on("connection", (ws, req) => {
     var userID = req.headers.user //get userid from URL ip:6060/userid
     webSockets[userID] = ws //add new user to the connection list
     ws.on('message', message => { //if there is any message
-      var datastring = message.toString();
-      console.log(datastring);
-  
-      if (datastring.charAt(0) == "{") {
-        datastring = datastring.replace(/\'/g, '"');
-        var data = JSON.parse(datastring)
-        // if (data.auth == "chatapphdfgjd34534hjdfk") {
-          // if (data.cmd == 'send') {
+        var datastring = message.toString();
+        console.log(datastring);
+
+        if (datastring.charAt(0) == "{") {
+            datastring = datastring.replace(/\'/g, '"');
+            var data = JSON.parse(datastring)
+            // if (data.auth == "chatapphdfgjd34534hjdfk") {
+            // if (data.cmd == 'send') {
             console.log("data.remoteId", data.remoteId);
             var boardws = webSockets[data.remoteId] //check if there is reciever connection
             if (boardws) {
-              boardws.send(datastring); //send message to reciever
-              ws.send("success");
+                boardws.send(datastring); //send message to reciever
+                ws.send("success");
             } else {
-            //   const message = {
-            //     notification: {
-            //       title: 'Test notification',
-            //       body: datastring
-            //     },
-            //     token: 'dCbH4pNNTiOFcH10G0_emA:APA91bEETF9Kq7nKX--2ClYuSRynjr9cAUr0fUeu5oEgQ10BTITL7qaKaAIrQa8E4h9IweureTsfobIla8QxAUgg7JWsinq_Okc-5DdGsCvfALtoDCLMqaxWzGb9jAh0o0QotfMU3-LI'
-            //   };
-            //   // Send the message to the device with the given registration token
-            //   messaging.send(message)
-            //     .then((response) => {
-            //       console.log('Successfully sent message:', response);
-            //     })
-            //     .catch((error) => {
-            //       console.log('Error sending message:', error);
-            //     });
-              console.log("No reciever user found.");
-              ws.send(data.cmd + ":No reciever user found");
+                //   const message = {
+                //     notification: {
+                //       title: 'Test notification',
+                //       body: datastring
+                //     },
+                //     token: 'dCbH4pNNTiOFcH10G0_emA:APA91bEETF9Kq7nKX--2ClYuSRynjr9cAUr0fUeu5oEgQ10BTITL7qaKaAIrQa8E4h9IweureTsfobIla8QxAUgg7JWsinq_Okc-5DdGsCvfALtoDCLMqaxWzGb9jAh0o0QotfMU3-LI'
+                //   };
+                //   // Send the message to the device with the given registration token
+                //   messaging.send(message)
+                //     .then((response) => {
+                //       console.log('Successfully sent message:', response);
+                //     })
+                //     .catch((error) => {
+                //       console.log('Error sending message:', error);
+                //     });
+                console.log("No reciever user found.");
+                ws.send(data.cmd + ":No reciever user found");
             }
-          // } else {
-          //   console.log("No send command");
-          //   ws.send(data.cmd + ":error");
-          // }
-        // } else {
-        //   console.log("App Authincation error");
-        //   ws.send(data.cmd + ":error");
-        // }
-      } else {
-        console.log("Non JSON type data");
-        ws.send(data.cmd + ":error");
-      }
+            // } else {
+            //   console.log("No send command");
+            //   ws.send(data.cmd + ":error");
+            // }
+            // } else {
+            //   console.log("App Authincation error");
+            //   ws.send(data.cmd + ":error");
+            // }
+        } else {
+            console.log("Non JSON type data");
+            ws.send(data.cmd + ":error");
+        }
     })
     ws.on("close", () => {
-      ws.send("User disconnected"+ userID);
-      users.delete(userID);
+        console.log("User disconnected", users);
+        delete webSockets[userID] //remove user connection
     });
-  });
+});
 
 // Add CORS middleware
 app.use(cors());
