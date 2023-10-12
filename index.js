a651e08304414e3686f5c5d38f2eab9b35be5a95
chandjr00 <abdulmoiz@vacon.ai>
2023-10-12 13:23:25 +0500

    ccc

--- index.js
+++ index.js
@@ -51,8 +51,13 @@ wss.on("connection", (ws, req) => {
                     notification: {
                         title: 'Test notification',
                         body: data?.text,
-                        channel_id : "my_channel_id"
                         // how can i send ma json data to notification 
+                    },
+                    android: {
+                        notification: {
+
+                            channel_id: "my_channel_id",
+                        }
                     },
                     token: 'cMQuo8q1RBGD9FmyyqqBfp:APA91bFubwMUHBxKHgb5KFj8gcKJejYmCZX-CLrZOu8-bzEhEiPvO3HEYCN_SUwFrgteir6UPAuaDS5PfZh7opvZ2QS3yuu58LxRN9KJ1YoFYi_F5C-iigDg2-4lM9J0K_bBntWuKsMc',
                     data: {
