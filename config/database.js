6191c00d446553345ff84fd707b08e6ddda7af24
irtza mazhar <irtza.mazhar@outgive.ca>
2023-11-06 13:54:47 +0500

    bilawal changes

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+
 });
 
 sequelize
@@ -13,6 +14,7 @@ sequelize
   })
   .catch((error) => {
     console.error('Unable to connect to the database:', error);
+    
   });
 
 module.exports = sequelize;
