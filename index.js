2c7a187f73e241d62a5e763b86821ebf23ff737b
awais773 <awaissafdar773@gmail.com>
2024-03-14 15:50:14 +0500

    update

--- index.js
+++ index.js
@@ -4,22 +4,9 @@ const http = require("http");
 const WebSocket = require("ws");
 const appRoutes = require('./routes/index');
 const db = require('./config/database');
-const app = express();
-const server = http.createServer(app);
-
-
-
-const wss = new WebSocket.Server({ server });
-
-
-
-const users = new Map();
-var webSockets = {}
 const admin = require('firebase-admin');
 const serviceAccount = require('./ab-chat-ca7a7-firebase-adminsdk-g0sp7-c3408c6a1d.json');
 const User = require('./model/User');
-
-
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -30,6 +17,7 @@ const messaging = admin.messaging();
 const url = require('url');
 
 const groups = {}; // Map to store group members
+const webSockets = {}; // Map to store WebSocket connections
 
 // Function to add user to a group
 function addUserToGroup(groupId, userId) {
@@ -51,6 +39,10 @@ function removeUserFromGroup(groupId, userId) {
     }
 }
 
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
 wss.on("connection", (ws, req) => {
     const reqUrl = url.parse(req.url, true);
     const userID = reqUrl.query.userID;
@@ -58,31 +50,30 @@ wss.on("connection", (ws, req) => {
     webSockets[userID] = ws;
 
     ws.on('message', async message => {
-        var datastring = message.toString();
-        console.log(datastring);
+        console.log('Received:', message);
 
-        if (datastring.charAt(0) == "{") {
-            datastring = datastring.replace(/\'/g, '"');
-            var data = JSON.parse(datastring);
-
+        // Check if it's a JSON string
+        try {
+            var data = JSON.parse(message);
+            
             // Check if it's a group message
             if (data.groupId && groups[data.groupId]) {
                 // Broadcast message to all members of the group
                 groups[data.groupId].forEach(memberId => {
                     const memberSocket = webSockets[memberId];
                     if (memberSocket && memberId !== data.author.id) {
-                        memberSocket.send(datastring);
+                        memberSocket.send(message);
                     }
                 });
             } else if (data.remoteId) {
                 var boardws = webSockets[data.remoteId];
                 if (boardws) {
-                    boardws.send(datastring);
+                    boardws.send(message);
                     ws.send("success");
                 } else {
                     const reciever = await User.findByPk(data.remoteId);
                     const sender = await User.findByPk(data.author.id);
-                    const message = {
+                    const notification = {
                         notification: {
                             title: sender.name,
                             body: data?.text,
@@ -103,27 +94,27 @@ wss.on("connection", (ws, req) => {
                             author: data.author.id,
                         }
                     };
-                    messaging.send(message)
+                    messaging.send(notification)
                         .then((response) => {
                             console.log('Successfully sent message:', response);
                         })
                         .catch((error) => {
                             console.log('Error sending message:', error);
                         });
-                    ws.send("No reciever user found");
+                    ws.send("No receiver user found");
                 }
             } else {
                 console.log("Invalid message format.");
                 ws.send("Invalid message format.");
             }
-        } else {
-            console.log("Non-JSON type data");
-            ws.send("Error: Non-JSON type data");
+        } catch (error) {
+            console.log("Error parsing JSON:", error);
+            ws.send("Error: Invalid JSON data");
         }
     });
 
     ws.on("close", () => {
-        console.log("User disconnected", users);
+        console.log("User disconnected");
         // Remove user from all groups
         for (let groupId in groups) {
             removeUserFromGroup(groupId, userID);
@@ -131,7 +122,6 @@ wss.on("connection", (ws, req) => {
         delete webSockets[userID];
     });
 });
-
 
 // Add CORS middleware
 app.use(cors());
