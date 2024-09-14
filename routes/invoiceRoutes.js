dbc3a193ef0d1ae00d7a48a04ad4f2f815d67171
awais773 <92942639+awais773@users.noreply.github.com>
2024-09-13 23:39:35 -0700

    Update invoiceRoutes.js
    awais

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const InvoiceController = require("../controller/InvoiceController")
 const { authentications } = require('../middleware/authentication');
+const { paginationMiddleware } = require('../middleware/pagination');
+
 
 
 const router = express.Router();
 
 router.post('/create', authentications, InvoiceController.create);
-router.get('/get', authentications, InvoiceController.get);
+router.get('/get', authentications,paginationMiddleware, InvoiceController.get);
 router.get('/get/:Id', authentications, InvoiceController.find);
 router.put('/update/:Id', authentications, InvoiceController.update);
 router.delete('/delete/:id', authentications, InvoiceController.Delete);
